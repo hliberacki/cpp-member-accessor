56be9e235d6ac512af80a45e55461a2866f46c37
Hubert Liberacki <hliberacki@gmail.com>
2018-10-22 14:58:36 +0200

    Enhane library usage and cmake file
    Added helper functions for accessing resources, functions and members.
    Change folder structure to make it more intuitive

--- include/accessor.hpp
+++ include/accessor.hpp
@@ -9,6 +9,7 @@
 #ifndef ACCESSOR_SRC_ACCESSOR_HPP
 #define ACCESSOR_SRC_ACCESSOR_HPP
 
+#include <functional>
 
 namespace accessor
 {
@@ -24,27 +25,40 @@ namespace accessor
       using type = R (C::*)(Args...);
     };
 
-    template<class T>
+    template<class Tag, class T>
     struct Proxy
     {
       static typename T::type value;
     };
 
-    template <class T>
-    typename T::type Proxy<T>::value;
+    template <class Tag, class T>
+    typename T::type Proxy<Tag, T>::value;
 
     template<class T, typename T::type AccessPointer>
     class MakeProxy
     {
-        struct Setter { Setter() { Proxy<T>::value = AccessPointer; } };
-        static Setter instance;
+      struct Setter { Setter() { Proxy<T, T>::value = AccessPointer; } };
+      static Setter instance;
     };
 
     template<class T, typename T::type AccessPointer>
     typename MakeProxy<T, AccessPointer>::Setter MakeProxy<T, AccessPointer>::instance;
 
     template<class T>
-    const auto accessEntity = Proxy<T>::value;
+    const auto accessEntity = Proxy<T, T>::value;
+
+    template<typename Sig, class Instance, typename... Args>
+    auto accessFunction(Instance & instance, Args ...args)
+    {
+      return (instance.*accessEntity<Sig>)(args...);
+    }
+
+    template<typename Sig, class Instance>
+    auto accessMember(Instance & instance)
+    {
+        return std::ref(instance.*accessEntity<Sig>);
+    }
+
 }
 
 #endif // ACCESSOR_SRC_ACCESSOR_HPP
--- tests/main.cpp
+++ tests/main.cpp
@@ -19,11 +19,33 @@ class Custom
 };
 
 struct CustomF : accessor::FunctionWrapper<void, Custom> {};
+struct CustomM : accessor::MemberWrapper<int, Custom> {};
+
 template class accessor::MakeProxy<CustomF, &Custom::privateF>;
+template class accessor::MakeProxy<CustomM, &Custom::mPriv>;
+
 
 int main()
 {
-  Custom c;
-  (c.*accessor::accessEntity<CustomF>)();
+  Custom c1, c2;
+
+  std::cout << "Accessing C1 object" << "\n\n";
+
+  std::cout << "privateF output:    ";
+  accessor::accessFunction<CustomF>(c1);
+
+  std::cout << "C1.mPrivate value:    "<< accessor::accessMember<CustomM>(c1) << "\n\n";
+
+  std::cout << "Changing C1 C2 private member values" << "\n\n";
+  auto c1Member = accessor::accessMember<CustomM>(c1);
+  c1Member.get() = 1000;
+
+  std::cout << "New C1.mPrivate value: " << accessor::accessMember<CustomM>(c1) << '\n';
+
+  auto c2Member = accessor::accessMember<CustomM>(c2);
+  c2Member.get() = 2000;
+
+  std::cout << "New C2.mPrivate value: " << accessor::accessMember<CustomM>(c2) << '\n';
+
   return 0;
 }
